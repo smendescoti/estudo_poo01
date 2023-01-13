a01cd3cbe4616718d24694bb4a092f7d42fced8f
smendescoti <sergio.coti@gmail.com>
2023-01-13 12:48:53 -0300

    first commit

---
+++ ProjetoAula01/Entities/Cliente.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//localização da classe dentro do projeto
+namespace ProjetoAula01.Entities
+{
+    //declaração da classe
+    public class Cliente
+    {
+        //atributos da classe (dados/campos)
+        private Guid _id;
+        private string _nome;
+        private string _telefone;
+        private string _email;
+        private string _cpf;
+
+        //métodos da classe (funções para encapsulamento)
+        public Guid Id {
+            set { _id = value; } //entrada
+            get { return _id; } //saída
+        }
+
+        public string Nome {
+            set { _nome = value; }  
+            get { return _nome; }
+        }
+
+        public string Telefone {
+            set { _telefone = value; }
+            get { return _telefone; }
+        }
+
+        public string Email {
+            set { _email = value; }
+            get { return _email; }
+        }
+
+        public string Cpf {
+            set { _cpf = value; }
+            get { return _cpf; }
+        }
+    }
+}
---
+++ ProjetoAula01/Repositories/ClienteRepository.cs
@@ -0,0 +1,30 @@
+﻿using ProjetoAula01.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//define a localização da classe
+namespace ProjetoAula01.Repositories
+{
+    //Declaração da classe
+    public class ClienteRepository
+    {
+        //método para receber um objeto do tipo Cliente
+        //e gravar os seus dados em um arquivo TXT
+        public void ExportarDados(Cliente cliente)
+        {
+            //abrindo um arquivo novo para escrita
+            using (var streamWriter = new StreamWriter($"c:\\clientes\\cliente_{cliente.Id}.txt"))
+            {
+                //escrevendo o conteudo do arquivo..
+                streamWriter.WriteLine($"ID DO CLIENTE...: {cliente.Id}");
+                streamWriter.WriteLine($"NOME............: {cliente.Nome}");
+                streamWriter.WriteLine($"TELEFONE........: {cliente.Telefone}");
+                streamWriter.WriteLine($"EMAIL...........: {cliente.Email}");
+                streamWriter.WriteLine($"CPF.............: {cliente.Cpf}");
+            }
+        }
+    }
+}
